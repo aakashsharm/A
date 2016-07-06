d1014994e753d084baf24ce2e362907aa2eec666
aakashsharm <aakashsharma.it@gmail.com>
2016-07-06 10:22:43 +0530

    second

--- command.bat
+++ command.bat
@@ -1,8 +1,10 @@
+cd C:/Users/aakashsharma/Desktop/github/command.bat/
+
 git status
 
-git clone https://github.com/aakashsharm/A
-
 git add *
+
+git clone https://github.com/aakashsharm/A
 
 git status
 
